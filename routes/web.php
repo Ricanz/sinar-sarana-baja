234b921325704239589e6ee4320a21334414e739
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-08-01 22:00:46 +0700

    admin

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AboutUsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,6 +17,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('landing-page.index');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.index');
+});
+
+Route::get('admin/about-us', [AboutUsController::class, 'index']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
