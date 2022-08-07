b959094e5d26228845f86bbf926dcd93abdd11f7
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-08-07 13:26:37 +0700

    toglle content

--- resources/views/landing-page/partials/header.blade.php
+++ resources/views/landing-page/partials/header.blade.php
@@ -1,4 +1,3 @@
-
 <header>
     <nav class="dekstop-nav">
         <div class="logo-ssb">
@@ -80,7 +79,7 @@
             <img src="{{ asset('custom/landing-page/images/ssb-logo.png') }}" alt="">
         </div>
         <div class="items">
-            <div class="toggle">
+            <div class="toggle" id="myBtn">
                 <img src="{{ asset('custom/landing-page/images/iconbase.png') }}" alt="">
             </div>
             <div class="search-box">
@@ -88,5 +87,116 @@
             </div>
 
         </div>
+        <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <span class="close">&times;</span>
+                <div class="nav-menu">
+                    <div class="mob-menu">
+                        <div class="menu">
+                            <h5 class="mb-10">About</h5>
+                            <ul>
+                                <a href="{{ url('about-us') }}">
+                                    <li>Profile</li>
+                                </a>
+                                <a href="{{ url('about-us#vission-mission') }}">
+                                    <li>Vision, Missiion & Values</li>
+                                </a>
+                                <li>Sertifikat/Legalitas</li>
+                            </ul>
+                        </div>
+                        <div class="menu">
+                            <h5 class="mb-10" onclick="footerDropdown('brosur')">Brosur<i
+                                    class="fa-solid fa-chevron-right mob-icon brosur"></i></h5>
+                            <ul class="menu-dropdown-brosur">
+                                <li>Company Profile</li>
+                                <li>Katalog Produk CSP</li>
+                                <li>Katalog Produk
+                                    Steel Bridge</li>
+                                <li>Katalog Produk
+                                    Perlengkapan Jalan</li>
+                                <li>Katalog Steel Pipe</li>
+                                <li>Katalog Bronjong</li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="mob-menu">
+                        <div class="menu">
+                            <h5 class="mb-10" onclick="footerDropdown('produk')">Produk<i
+                                    class="fa-solid fa-chevron-right mob-icon produk"></i></h5>
+                            <ul class="menu-dropdown-produk">
+                                <li>Nestable Flange E-100</li>
+                                <li>Multi Plate (sub lagi 13 tipe)</li>
+                                <li>Flex Beam Guardrail & Railing</li>
+                                <li>Tiang PJU</li>
+                                <li>Steel Bridge</li>
+                                <li>Steel Pipe</li>
+                                <li>Bronjong</li>
+                            </ul>
+                        </div>
+                        <div class="menu">
+                            <h5 class="mb-10" onclick="footerDropdown('jasa')">Jasa<i
+                                    class="fa-solid fa-chevron-right mob-icon jasa"></i></h5>
+                            <ul class="menu-dropdown-jasa">
+                                <li>Landing</li>
+                                <li>Jasa Perakitan</li>
+                                <li>Jasa Pekerjaan Sipil</li>
+                                <li>Jasa Rancang Bangun</li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+                {{-- <div class="col-4 mob-menu">
+                    <div class="menu">
+                        <h5 class="mb-10" onclick="footerDropdown('kontak')">Kontak<i
+                                class="fa-solid fa-chevron-right mob-icon kontak"></i></h5>
+                        <ul class="menu-dropdown-kontak">
+                            <li>Lokasi</li>
+                            <li>Hunting : nomor marketing</li>
+                            <li>Nomor Gigih/Dito</li>
+                            <li>Nomor Dhendi/Budi</li>
+                            <li>Nomor Herdyan/Santoso</li>
+                            <li>Nomor Anton/Gress</li>
+                        </ul>
+                    </div>
+                    <div class="menu">
+                        <h5 class="mb-10" onclick="footerDropdown('klien')">Klien<i
+                                class="fa-solid fa-chevron-right mob-icon klien"></i></h5>
+                        <ul class="menu-dropdown-klien">
+                            <li>Nama Klien</li>
+                        </ul>
+                    </div>
+                </div> --}}
+            </div>
+
+        </div>
     </nav>
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
 </header>
