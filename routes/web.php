7a7cfa03fdefe78fd5046bd6920940ad8d45b0da
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-08-09 22:22:50 +0700

    Revert "add breeze"
    This reverts commit d70fa851a08a8ad49279d995c3baaa10405d5948.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\AboutUsController;
+use App\Http\Controllers\GeneralController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('landing-page.index');
 });
+
+Route::get('/admin/dashboard', function () {
+    return view('admin.index');
+});
+
+Route::get('admin/about-us', [AboutUsController::class, 'index']);
+Route::get('/about-us', [GeneralController::class, 'about']);
+Route::get('/news', [GeneralController::class, 'news']);
+Route::get('/news-detail/{slug}', [GeneralController::class, 'news_detail']);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
