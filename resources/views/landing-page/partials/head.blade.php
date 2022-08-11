058d454ce133ab62a7c2a196661d33e711cfdf68
ctriyanti <riyanti@cashtree.id>
2022-08-11 09:33:18 +0700

    update meta

--- resources/views/landing-page/partials/head.blade.php
+++ resources/views/landing-page/partials/head.blade.php
@@ -1,13 +1,37 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
-    <meta name="description" content="
-        PT. Sinergy Sarana Baja merupakan perusahaan nasional yang berdiri dengan dilatarbelakangi oleh pesatnya pertumbuhan proyek infrastruktur di Indonesia yang merupakan salah satu faktor penggerak ekonomi nasional khususnya pada proyek konstruksi baja. Tingginya permintaan pasar terhadap produk baja khususnya Jembatan Rangka Baja / Steel Bridge, Pipa Baja Bergelombang/Corrugated Steel Pipe, Pagar Pengaman Jalan/Flex Beam Guardrail, Fasilitas Perlengkapan Jalan Raya, Gabion serta produk konstruksi baja lainnya, membuat PT. Sinergy Sarana Baja ikut turut berpartisipasi dalam kegiatan pembangunan infrastruktur di Indonesia.
-    ">
+
     <meta name="keywords" content="ssb, sinergy, sarana, baja">
     <meta name="author" content="Sinergy Sarana Baja">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+
     <title>Sinergy Sarana Baja</title>
+    <meta property="og:url" content="{{request()->url()}}">
+    <meta property="og:type" content="baja" />
+    <meta property="og:title" content="Sinergy Sarana Baja" />
+    <meta property="og:description" content="PT. Sinergy Sarana Baja merupakan perusahaan nasional yang berdiri dengan dilatarbelakangi oleh pesatnya pertumbuhan proyek infrastruktur di Indonesia yang merupakan salah satu faktor penggerak ekonomi nasional khususnya pada proyek konstruksi baja. Tingginya permintaan pasar terhadap produk baja khususnya Jembatan Rangka Baja / Steel Bridge, Pipa Baja Bergelombang/Corrugated Steel Pipe, Pagar Pengaman Jalan/Flex Beam Guardrail, Fasilitas Perlengkapan Jalan Raya, Gabion serta produk konstruksi baja lainnya, membuat PT. Sinergy Sarana Baja ikut turut berpartisipasi dalam kegiatan pembangunan infrastruktur di Indonesia." />
+    <meta property="og:image" content="{{ asset('custom/landing-page/images/ssb-logo.png') }}" />
+
+    <meta name="title" content="Sinergy Sarana Baja">
+    <meta name="description" content="
+        PT. Sinergy Sarana Baja merupakan perusahaan nasional yang berdiri dengan dilatarbelakangi oleh pesatnya pertumbuhan proyek infrastruktur di Indonesia yang merupakan salah satu faktor penggerak ekonomi nasional khususnya pada proyek konstruksi baja. Tingginya permintaan pasar terhadap produk baja khususnya Jembatan Rangka Baja / Steel Bridge, Pipa Baja Bergelombang/Corrugated Steel Pipe, Pagar Pengaman Jalan/Flex Beam Guardrail, Fasilitas Perlengkapan Jalan Raya, Gabion serta produk konstruksi baja lainnya, membuat PT. Sinergy Sarana Baja ikut turut berpartisipasi dalam kegiatan pembangunan infrastruktur di Indonesia.
+    ">
+
+
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="{{request()->url()}}">
+    <meta property="og:title" content="Sinergy Sarana Baja">
+    <meta property="og:description" content="PT. Sinergy Sarana Baja merupakan perusahaan nasional yang berdiri dengan dilatarbelakangi oleh pesatnya pertumbuhan proyek infrastruktur di Indonesia yang merupakan salah satu faktor penggerak ekonomi nasional khususnya pada proyek konstruksi baja. Tingginya permintaan pasar terhadap produk baja khususnya Jembatan Rangka Baja / Steel Bridge, Pipa Baja Bergelombang/Corrugated Steel Pipe, Pagar Pengaman Jalan/Flex Beam Guardrail, Fasilitas Perlengkapan Jalan Raya, Gabion serta produk konstruksi baja lainnya, membuat PT. Sinergy Sarana Baja ikut turut berpartisipasi dalam kegiatan pembangunan infrastruktur di Indonesia.">
+    <meta property="og:image" content="{{ asset('custom/landing-page/images/ssb-logo.png') }}">
+
+
+    <meta property="twitter:card" content="summary_large_image">
+    <meta property="twitter:url" content="{{request()->url()}}">
+    <meta property="twitter:title" content="Sinergy Sarana Baja">
+    <meta property="twitter:description" content="PT. Sinergy Sarana Baja merupakan perusahaan nasional yang berdiri dengan dilatarbelakangi oleh pesatnya pertumbuhan proyek infrastruktur di Indonesia yang merupakan salah satu faktor penggerak ekonomi nasional khususnya pada proyek konstruksi baja. Tingginya permintaan pasar terhadap produk baja khususnya Jembatan Rangka Baja / Steel Bridge, Pipa Baja Bergelombang/Corrugated Steel Pipe, Pagar Pengaman Jalan/Flex Beam Guardrail, Fasilitas Perlengkapan Jalan Raya, Gabion serta produk konstruksi baja lainnya, membuat PT. Sinergy Sarana Baja ikut turut berpartisipasi dalam kegiatan pembangunan infrastruktur di Indonesia.">
+    <meta property="twitter:image" content="{{ asset('custom/landing-page/images/ssb-logo.png') }}">
+
     <link rel="icon" type="image/x-icon" href="{{ asset('custom/landing-page/images/favicon.png') }}">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins">
     <link rel="stylesheet" href="{{ asset('custom/landing-page/css/style.css') }}">
