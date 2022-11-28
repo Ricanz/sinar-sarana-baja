d1f5ce0dcdd13afc3780b0a2cbdfd24a8bec44c7
Riyanti Maulya <riyantimaulya17@gmail.com>
2022-11-28 23:12:53 +0700

    primary map

--- resources/views/admin/contact/maps.blade.php
+++ resources/views/admin/contact/maps.blade.php
@@ -1,6 +1,6 @@
 <x-app-layout>
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
-        <h1 class="h3 mb-0 text-gray-800">Form Certificates</h1>
+        <h1 class="h3 mb-0 text-gray-800">Form Primary Contact</h1>
     </div>
     
     <form class="form-group" method="POST" action="{{ route('addMaps') }}" enctype="multipart/form-data">
