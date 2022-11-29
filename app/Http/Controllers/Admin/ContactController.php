1181c66530ac2ca84d149ed93630517562790d57
ctriyanti <riyanti@cashtree.id>
2022-11-29 11:40:07 +0700

    contacts data

--- app/Http/Controllers/Admin/ContactController.php
+++ app/Http/Controllers/Admin/ContactController.php
@@ -11,7 +11,7 @@ use Illuminate\Http\Request;
 class ContactController extends Controller
 {
     public function index(){
-        $contacts = Contact::all();
+        $contacts = Contact::where('status', 'active')->get();
         return view('admin/contact/index', compact('contacts'));
     }
 
